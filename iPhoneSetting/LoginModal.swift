61d4279dd3d58557973e6d040f52078c18f07b71
백대홍 <bdh3620@naver.com>
2023-01-08 15:11:19 +0900

    modalview nextbtn fix

--- iPhoneSetting/LoginModal.swift
+++ iPhoneSetting/LoginModal.swift
@@ -8,56 +8,70 @@
 import SwiftUI
 
 struct LoginModal: View {
+    enum Field: Hashable {
+        case email
+    }
+    
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @State var email: String = ""
+    @FocusState private var focusedField: Field?
     var body: some View {
-           VStack {
-               HStack {
-                   Button(action: {
-                       presentationMode.wrappedValue.dismiss()
-                   },label: {
-                       Text("취소")
-                           .padding(20)
-                   })
-                   Spacer()
-               }
-               VStack(spacing: 10){
-                   Text("Apple ID")
-                       .font(.title)
-                       
-                   Text("iCloud 및 기타 Apple 서비스에서 사용할 Apple ID로 로그인하십시오.").font(.title3)
-                   
-                   
-                   Spacer()
-                       .frame(height: 30)
-                   
-                   Divider()
-                   
-                   
-                   HStack(alignment: .center) {
-                       Text("Apple ID").bold()
-                       TextField("이메일", text: $email)
-                               .padding()
-                   
-                        
-                               
-                       }
+        VStack {
+            HStack {
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+                },label: {
+                    Text("취소")
+                        .padding(20)
+                })
+                Spacer()
                 
-               
-                   Divider()
-                   
-                       
-                   
-                       
-                   
-                   
-                   
-               }
-               .padding()
-               Spacer()
-           }
-       }
-   }
+                Button("다음") {
+                    if email.isEmpty {
+                        focusedField = .email
+                    }
+                }
+                
+                .padding()
+                
+            }
+            Spacer()
+            
+            VStack(spacing: 10){
+                Text("Apple ID")
+                    .font(.title)
+                
+                Text("iCloud 및 기타 Apple 서비스에서 사용할 Apple ID로 로그인하십시오.")
+                    .font(.title3)
+                    .padding()
+                
+                
+                Spacer()
+                    .frame(height: 10)
+                
+                Divider()
+                
+                HStack(alignment: .center, spacing: 20) {
+                    Text("Apple ID").bold()
+                    TextField("이메일", text: $email)
+                        
+                        .focused($focusedField, equals: .email)
+                    
+                    
+                    
+                }
+                .padding()
+                
+                Divider()
+                
+            Spacer()
+            }
+            
+        }
+        
+        
+    }
+}
 struct LoginModal_Previews: PreviewProvider {
     static var previews: some View {
         LoginModal()
