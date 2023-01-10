cf754795348bab4796554d02c151feb4fd7d8b2d
백대홍 <bdh3620@naver.com>
2023-01-10 18:20:54 +0900

    PictureView 추가 및 수정 진행 중

---
+++ iPhoneSetting/PictureView.swift
@@ -0,0 +1,20 @@
+//
+//  PictureView.swift
+//  iPhoneSetting
+//
+//  Created by 백대홍 on 2023/01/10.
+//
+
+import SwiftUI
+
+struct PictureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PictureView_Previews: PreviewProvider {
+    static var previews: some View {
+        PictureView()
+    }
+}
